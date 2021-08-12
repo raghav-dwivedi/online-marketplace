42711423705e0a4d481c487e6dd6dd32118aad88
Raghav Dwivedi <rd9008000@gmail.com>
2021-08-13 00:55:36 +0530

    added env helmet compression logging ssl

--- app.js
+++ app.js
@@ -1,45 +1,55 @@
 const path = require('path');
 const fs = require('fs');
+const https = require('https');
 
 const express = require('express');
-const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const session = require('express-session');
 const MongoDBStore = require('connect-mongodb-session')(session);
 const csrf = require('csurf');
 const flash = require('connect-flash');
 const multer = require('multer');
+const helmet = require('helmet');
+const compression = require('compression');
+const morgan = require('morgan');
 
 const errorController = require('./controllers/error');
 const User = require('./models/user');
 
-const MONGODB_URI = 'mongodb+srv://raghav:IqVmtHF4WEcDKkOJ@cluster0.bhzjg.mongodb.net/shop';
+const MONGODB_URI = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.bhzjg.mongodb.net/${process.env.MONGO_DEFAULT_DATABASE}`;
 
 const app = express();
 const store = MongoDBStore({
-    uri: MONGODB_URI,
-    collection: 'sessions'
+	uri: MONGODB_URI,
+	collection: 'sessions',
 });
 const csrfProtection = csrf();
 
+// const privateKey = fs.readFileSync('server.key');
+// const certificate = fs.readFileSync('server.cert');
+
 const fileStorage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        fs.mkdirSync('images', {
-            recursive: true
-        });
-        cb(null, 'images');
-    },
-    filename: (req, file, cb) => {
-        cb(null, Date.now() + '-' + file.originalname);
-    }
+	destination: (req, file, cb) => {
+		fs.mkdirSync('images', {
+			recursive: true,
+		});
+		cb(null, 'images');
+	},
+	filename: (req, file, cb) => {
+		cb(null, Date.now() + '-' + file.originalname);
+	},
 });
 
 const fileFilter = (req, file, cb) => {
-    if (file.mimetype === 'image/png' || file.mimetype === 'image/jpg' || file.mimetype === 'image/jpeg') {
-        cb(null, true);
-    } else {
-        cb(null, false);
-    }
+	if (
+		file.mimetype === 'image/png' ||
+		file.mimetype === 'image/jpg' ||
+		file.mimetype === 'image/jpeg'
+	) {
+		cb(null, true);
+	} else {
+		cb(null, false);
+	}
 };
 
 app.set('view engine', 'pug');
@@ -49,47 +59,61 @@ const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 const authRoutes = require('./routes/auth');
 
+const accessLogStream = fs.createWriteStream(
+	path.join(__dirname, 'access.log'),
+	{ flags: 'a' }
+);
 
-app.use(bodyParser.urlencoded({
-    extended: false
-}));
-app.use(multer({
-    storage: fileStorage,
-    fileFilter: fileFilter
-}).single('image')); // because parameter name is image in the view
+app.use(helmet());
+app.use(compression());
+app.use(morgan('combined', { stream: accessLogStream }));
+
+app.use(
+	express.urlencoded({
+		extended: false,
+	})
+);
+app.use(
+	multer({
+		storage: fileStorage,
+		fileFilter: fileFilter,
+	}).single('image')
+); // because parameter name is image in the view
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'images')));
-app.use(session({
-    secret: 'my secret',
-    resave: false,
-    saveUninitialized: false,
-    store: store
-}));
+app.use(
+	session({
+		secret: 'my secret',
+		resave: false,
+		saveUninitialized: false,
+		store: store,
+	})
+);
 app.use(csrfProtection);
 app.use(flash());
 
 app.use((req, res, next) => {
-    if (req.session.user) {
-        User.findById(req.session.user._id)
-            .then(user => {
-                if (!user) {
-                    return next();
-                }
-                req.user = user;
-                next();
-            })
-            .catch(err => {
-                throw new Error(err);
-            });
-    } else {
-        next();
-    }
+	if (req.session.user) {
+		User.findById(req.session.user._id)
+			.then((user) => {
+				if (!user) {
+					return next();
+				}
+				req.user = user;
+				next();
+			})
+			.catch((err) => {
+				throw new Error(err);
+			});
+	} else {
+		next();
+	}
 });
 
 app.use((req, res, next) => {
-    res.locals.isAuthenticated = req.session.isLoggedIn;
-    res.locals.csrfToken = req.csrfToken();
-    next();
+	res.locals.isAuthenticated = req.session.isLoggedIn;
+	res.locals.csrfToken = req.csrfToken();
+	next();
 });
 
 app.use('/admin', adminRoutes);
@@ -101,22 +125,32 @@ app.get('/500', errorController.get500);
 app.use(errorController.get404);
 
 app.use((error, req, res, next) => {
-    console.log(error);
-    res.redirect('/500');
+	console.log(error);
+	res.redirect('/500');
 });
 
-mongoose.connect(MONGODB_URI, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    })
-    .then(result => {
-        app.listen(3000);
-    })
-    .catch(err => {
-        const error = new Error(err);
-        error.httpStatusCode = 500;
-        return next(error);
-    });
+mongoose
+	.connect(MONGODB_URI, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	})
+	.then((result) => {
+		// https
+		// 	.createServer(
+		// 		{
+		// 			key: privateKey,
+		// 			cert: certificate,
+		// 		},
+		// 		app
+		// 	)
+		// 	.listen(process.env.PORT || 3000);
+		app.listen(process.env.PORT || 3000);
+	})
+	.catch((err) => {
+		const error = new Error(err);
+		error.httpStatusCode = 500;
+		return next(error);
+	});
 
 /*
  const routes = require('./routes-Section3')
